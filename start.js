33258253e18b84b445a1b1d67572462bf649b99a
Utsav0831 <utsav0920@gmail.com>
2023-07-24 00:42:17 +0545

    SISSA

--- start.js
+++ start.js
@@ -1,26 +1,31 @@
-function register(){
-    document.querySelector("#aregister a").addEventListener("click", function(){
-        document.querySelector("#signin").style.display = "none";
-        document.querySelector("#signin").style.right = "99%";
-        document.querySelector("#signup").style.display = "flex";
-        document.querySelector("body").style.backgroundPosition = "90%";
+let intervalId;
 
-        setInterval(function(){
-            document.querySelector("#signup").style.left = "7%";
-        },50);
-        
-    });
+function register() {
+  document.querySelector("#aregister a").addEventListener("click", function () {
+    clearInterval(intervalId); // Clear previous interval, if any
+    document.title = "SISSA - Register";
+    document.querySelector("#signin").style.display = "none";
+    document.querySelector("#signin").style.right = "99%";
+    document.querySelector("#signup").style.display = "flex";
+    document.querySelector("body").style.backgroundPosition = "90%";
+
+    intervalId = setInterval(function () {
+      document.querySelector("#signup").style.left = "7%";
+    }, 50);
+  });
 }
 
-function login(){
-    document.querySelector("#alogin a").addEventListener("click", function(){
-        document.querySelector("#signin").style.display = "flex";
-        document.querySelector("#signup").style.display = "none";
-        document.querySelector("#signup").style.left = "99%";
-        document.querySelector("body").style.backgroundPosition = "";
+function login() {
+  document.querySelector("#alogin a").addEventListener("click", function () {
+    clearInterval(intervalId); // Clear previous interval, if any
+    document.title = "SISSA - Log In";
+    document.querySelector("#signin").style.display = "flex";
+    document.querySelector("#signup").style.display = "none";
+    document.querySelector("#signup").style.left = "99%";
+    document.querySelector("body").style.backgroundPosition = "";
 
-        setInterval(function(){
-            document.querySelector("#signin").style.right = "7%";
-        },50);
-    });
+    intervalId = setInterval(function () {
+      document.querySelector("#signin").style.right = "7%";
+    }, 50);
+  });
 }
